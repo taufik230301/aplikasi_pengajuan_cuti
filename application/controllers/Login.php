a51ae911c44194c487e872c844901768a52a3492
Taufiiqul Hakim <51141717+taufik230301@users.noreply.github.com>
2022-05-09 10:12:36 +0700

    add system to github

---
+++ application/controllers/Login.php
@@ -0,0 +1,19 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Login extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('login');
+	}
+
+	public function proses()
+	{
+		$username = $this->input->post("username");
+		$password = $this->input->post("password");
+
+		echo var_dump($username);
+		die();
+	}
+}
