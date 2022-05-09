16d34251da844e027b91e3f2666e6760a36bcd37
Taufiiqul Hakim <51141717+taufik230301@users.noreply.github.com>
2022-05-09 12:32:32 +0700

    add icon text pegawai dan admin:

--- application/views/super_admin/components/sidebar.php
+++ application/views/super_admin/components/sidebar.php
@@ -53,7 +53,6 @@
                         <p>Cuti</p>
                     </a>
                 </li>
-                
                 <li class="nav-item">
                     <a href="#" class="nav-link">
                         <i class="nav-icon fas fa-cog"></i>
