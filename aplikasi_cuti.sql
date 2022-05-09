aa1d6fdf351a6cfc9bcf01feda9626c68f322ed4
Taufiiqul Hakim <51141717+taufik230301@users.noreply.github.com>
2022-05-09 10:13:45 +0700

    add sql

---
+++ aplikasi_cuti.sql
@@ -0,0 +1,166 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 09, 2022 at 05:13 AM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 7.4.13
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `aplikasi_cuti`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cuti`
+--
+
+CREATE TABLE `cuti` (
+  `id_cuti` varchar(30) NOT NULL,
+  `id_user` varchar(256) NOT NULL,
+  `alasan` text NOT NULL,
+  `tgl_diajukan` date NOT NULL,
+  `mulai` date NOT NULL,
+  `berakhir` date NOT NULL,
+  `id_status_cuti` int(12) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jenis_kelamin`
+--
+
+CREATE TABLE `jenis_kelamin` (
+  `id_jenis_kelamin` int(11) NOT NULL,
+  `jenis_kelamin` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `status_cuti`
+--
+
+CREATE TABLE `status_cuti` (
+  `id_status_cuti` int(11) NOT NULL,
+  `status_cuti` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id_user` varchar(256) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(30) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `id_user_level` int(11) NOT NULL,
+  `id_user_detail` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_detail`
+--
+
+CREATE TABLE `user_detail` (
+  `id_user_detail` varchar(256) NOT NULL,
+  `jenis_kelamin` int(12) NOT NULL,
+  `no_telp` varchar(30) NOT NULL,
+  `alamat` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_level`
+--
+
+CREATE TABLE `user_level` (
+  `id_user_level` int(11) NOT NULL,
+  `user_level` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `cuti`
+--
+ALTER TABLE `cuti`
+  ADD PRIMARY KEY (`id_cuti`);
+
+--
+-- Indexes for table `jenis_kelamin`
+--
+ALTER TABLE `jenis_kelamin`
+  ADD PRIMARY KEY (`id_jenis_kelamin`);
+
+--
+-- Indexes for table `status_cuti`
+--
+ALTER TABLE `status_cuti`
+  ADD PRIMARY KEY (`id_status_cuti`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id_user`);
+
+--
+-- Indexes for table `user_detail`
+--
+ALTER TABLE `user_detail`
+  ADD PRIMARY KEY (`id_user_detail`);
+
+--
+-- Indexes for table `user_level`
+--
+ALTER TABLE `user_level`
+  ADD PRIMARY KEY (`id_user_level`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `jenis_kelamin`
+--
+ALTER TABLE `jenis_kelamin`
+  MODIFY `id_jenis_kelamin` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `status_cuti`
+--
+ALTER TABLE `status_cuti`
+  MODIFY `id_status_cuti` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user_level`
+--
+ALTER TABLE `user_level`
+  MODIFY `id_user_level` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
