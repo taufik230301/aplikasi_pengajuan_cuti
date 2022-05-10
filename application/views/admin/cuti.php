e86c8d978edd16a1982c6574f1ac156179816945
Taufiiqul Hakim <51141717+taufik230301@users.noreply.github.com>
2022-05-10 10:09:32 +0700

    fix navbar and text

--- application/views/admin/cuti.php
+++ application/views/admin/cuti.php
@@ -28,12 +28,12 @@
                 <div class="container-fluid">
                     <div class="row mb-2">
                         <div class="col-sm-6">
-                            <h1 class="m-0">Dashboard</h1>
+                            <h1 class="m-0">Cuti</h1>
                         </div><!-- /.col -->
                         <div class="col-sm-6">
                             <ol class="breadcrumb float-sm-right">
                                 <li class="breadcrumb-item"><a href="#">Home</a></li>
-                                <li class="breadcrumb-item active">Dashboard v1</li>
+                                <li class="breadcrumb-item active">Cuti</li>
                             </ol>
                         </div><!-- /.col -->
                     </div><!-- /.row -->
@@ -49,7 +49,7 @@
                         <div class="col-12">
                             <div class="card">
                                 <div class="card-header">
-                                    <h3 class="card-title">Data User UMKM</h3>
+                                    <h3 class="card-title">Data Cuti Pegawai</h3>
                                 </div>
                                 <!-- /.card-header -->
                                 <div class="card-body">
