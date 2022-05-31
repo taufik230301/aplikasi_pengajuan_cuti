a951e56831bc5848b9283990299759c20287ce9a
Taufiiqul Hakim <51141717+taufik230301@users.noreply.github.com>
2022-05-31 10:30:47 +0700

    add blue admin dashboard

--- application/views/super_admin/components/header.php
+++ application/views/super_admin/components/header.php
@@ -29,3 +29,5 @@
 <link rel="stylesheet" href="<?= base_url();?>assets/admin_lte/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
 <link rel="stylesheet" href="<?= base_url();?>assets/admin_lte/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
 <link rel="stylesheet" href="<?= base_url();?>assets/admin_lte/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
+<!-- Style CSS -->
+<link rel="stylesheet" href="<?= base_url();?>assets/style.css">
