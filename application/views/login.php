d9055a48865ec3f5e988b9bc14e762ee0fc5d3fa
Taufiiqul Hakim <51141717+taufik230301@users.noreply.github.com>
2022-06-02 17:23:56 +0700

    route login register

--- application/views/login.php
+++ application/views/login.php
@@ -133,8 +133,8 @@
                     </div>
 
                     <div class="text-center p-t-136">
-                        <a class="txt2" href="#">
-                            Create your Account
+                        <a class="txt2" href="<?=base_url();?>Register/index">
+                            Buat akun
                             <i class="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
                         </a>
                     </div>
--- application/views/register.php
+++ application/views/register.php
@@ -7,13 +7,16 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Sign Up Form by Colorlib</title>
 
+    <!--===============================================================================================-->
+    <link rel="icon" type="image/png" href="<?= base_url();?>assets/favicon.ico" />
+
     <!-- Font Icon -->
     <link rel="stylesheet"
         href="<?=base_url();?>assets/register/fonts/material-icon/css/material-design-iconic-font.min.css">
 
     <!-- Main css -->
     <link rel="stylesheet" href="<?=base_url();?>assets/register/css/style.css">
-    
+
     <!-- Sweetalert -->
     <script src="<?= base_url() ?>node_modules/sweetalert/dist/sweetalert.min.js"></script>
 </head>
@@ -40,26 +43,27 @@
                         <h2 class="form-title">Buat Akun</h2>
                         <div class="form-group">
                             <input type="text" class="form-input" name="username" id="username"
-                                placeholder="Your Username" required/>
+                                placeholder="Your Username" required />
                         </div>
                         <div class="form-group">
-                            <input type="email" class="form-input" name="email" id="email" placeholder="Your Email" required/>
+                            <input type="email" class="form-input" name="email" id="email" placeholder="Your Email"
+                                required />
                         </div>
                         <div class="form-group">
-                            <input type="text" class="form-input" name="password" id="password"
-                                placeholder="Password" required/>
+                            <input type="text" class="form-input" name="password" id="password" placeholder="Password"
+                                required />
                             <span toggle="#password" class="zmdi zmdi-eye field-icon toggle-password"></span>
                         </div>
                         <div class="form-group">
                             <input type="password" class="form-input" name="re_password" id="re_password"
-                                placeholder="Repeat your password" required/>
+                                placeholder="Repeat your password" required />
                         </div>
                         <div class="form-group">
                             <input type="submit" name="submit" id="submit" class="form-submit" value="Sign up" />
                         </div>
                     </form>
                     <p class="loginhere">
-                        Have already an account ? <a href="#" class="loginhere-link">Login here</a>
+                        Sudah ada akun ? <a href="<?=base_url();?>Login/index" class="loginhere-link">Login disini</a>
                     </p>
                 </div>
             </div>
