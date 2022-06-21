5d167dbff2439ea586d4854315be896cdd4339cc
Taufiiqul Hakim <51141717+taufik230301@users.noreply.github.com>
2022-06-21 13:34:17 +0700

    add date format

--- application/views/laporan_pdf.php
+++ application/views/laporan_pdf.php
@@ -8,6 +8,27 @@
 </head>
 
 <body>
+<?php
+function tgl_indo($tanggal){
+	$bulan = array (
+		1 =>   'Januari',
+		'Februari',
+		'Maret',
+		'April',
+		'Mei',
+		'Juni',
+		'Juli',
+		'Agustus',
+		'September',
+		'Oktober',
+		'November',
+		'Desember'
+	);
+	$pecahkan = explode('-', $tanggal);
+ 
+	return $pecahkan[2] . ' ' . $bulan[ (int)$pecahkan[1] ] . ' ' . $pecahkan[0];
+}
+?>
     <?php
 
 $id = 0;
@@ -33,7 +54,7 @@ $id_status_cuti = $i['id_status_cuti'];
             style="height: 92px; margin-left: auto; margin-right: auto; width: 200px; border: none; background-color: white;">
             <tbody>
                 <tr style="height: 113.575px;">
-                    <img style="margin-top:20px; margin-right:-100px;  margin-left:-70px;" src="<?=base_url();?>assets/login/images/sumsel.png" alt="" height="95" />
+                    <img style="margin-top:20px; margin-right:-100px;  margin-left:-80px;" src="<?=base_url();?>assets/login/images/sumsel.png" alt="" height="95" />
                     <td style="width: 470px; text-align: center; height: 113.575px; border: none;">
 
                         <h4 style="text-align: center; margin-bottom:-20px;">PEMERINTAH PROVINSI SUMATERA SELATAN</h4>
@@ -55,21 +76,21 @@ $id_status_cuti = $i['id_status_cuti'];
             &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
             &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
             &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Pagaralam,
-            <?=$tgl_diajukan?></p>
+            <?=tgl_indo($tgl_diajukan)?></p>
         <p style="margin-bottom:-12px">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
             &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
             &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
             &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
             Kepada&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
             &nbsp; &nbsp;</p>
-        <p style="margin-bottom:-12px">Nommor &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; :&nbsp; &nbsp;
+        <p style="margin-bottom:-12px">Nomor &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  &nbsp;:
             &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
             &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
             &nbsp; &nbsp; &nbsp;&nbsp;
             &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
             &nbsp; &nbsp; Yth. Kepala Badan
             Pendapatan Daerah</p>
-        <p style="margin-bottom:-12px">Lampiran&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+        <p style="margin-bottom:-12px">Lampiran&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; : &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
             &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
             &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; Provinsi
             Sumatera Selatan</p>
@@ -100,7 +121,7 @@ $id_status_cuti = $i['id_status_cuti'];
         <p style="text-align: justify">&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;Dikemukakan kepada Bapak/Ibu pada
             prinsipnya kami tidak berkeberatan atas usul cuti <?=$perihal_cuti?> pegawai tersebut, mengingat cuti
             <?=$perihal_cuti?> dimulai
-            terhitung dimulai pada <?= $mulai ?> s.d <?=$berakhir?> yang akan dipergunakan oleh yang berasangkutan untuk
+            terhitung dimulai pada <?= tgl_indo($mulai) ?> s.d <?=tgl_indo($berakhir)?> yang akan dipergunakan oleh yang berasangkutan untuk
             <?=$alasan?>. Dengan kami ajukan, namun segala pertimbangan kami serahkan kepada
             Bapak/Ibu atas perhatiannya dan perkenannya diucapkan terima kasih.</p>
         <p>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
